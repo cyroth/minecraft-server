1917956b9b5582a60fba2a142a8c4a9bcdae300b
cyroth <cyroth@users.noreply.github.com>
2017-10-28 00:14:46 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo yum install -y java-1.8.0-openjdk screen git -y
 sudo yum remove java-1.7.0-openjdk -y
 java -jar forge-*-installer.jar --installServer
-mv minecraft-server-setup/ops.json ./
-sudo mv minecraft-server-setup/minecraft.init /etc/init.d/minecraft
+mv minecraft-server-setup-aws/ops.json ./
+sudo mv minecraft-server-setup-aws/minecraft.init /etc/init.d/minecraft
 sudo chmod +x /etc/init.d/minecraft
 sudo chkconfig --add minecraft
 sudo chkconfig minecraft on
