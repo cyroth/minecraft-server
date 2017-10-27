53099e3d2f35a0f00ec1415e999f1bc7bd3ac878
cyroth <cyroth@users.noreply.github.com>
2017-10-28 00:10:31 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo yum install -y java-1.8.0-openjdk screen git -y
+sudo yum remove java-1.7.0-openjdk -y
+java -jar forge-*-installer.jar --installServer
+mv minecraft-server-setup/ops.json ./
+sudo mv minecraft-server-setup/minecraft.init /etc/init.d/minecraft
+sudo chmod +x /etc/init.d/minecraft
+sudo chkconfig --add minecraft
+sudo chkconfig minecraft on
+sudo service restart minecraft
