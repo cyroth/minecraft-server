5d264bb521b879651d16675c37e5f01f6af0d742
cyroth <github@cyroth.com>
2018-02-13 00:13:51 +1000

    updated installer

--- install-forge.sh
+++ install-forge.sh
@@ -8,6 +8,10 @@ sudo chmod +x /etc/init.d/minecraft
 sudo chkconfig --add minecraft
 sudo chkconfig minecraft on
 echo "Server installed!"
+echo "Doing initial startup to configure folders and sleeping for 30 seconds. It hasn't crashed (probably)"
+sudo service minecraft restart
+sleep 30
+sudo service minecraft stop
 echo "upload your mods or world and run:"
 echo "sudo service minecraft restart"
 echo "or sudo vim /etc/init.d/minecraft and select standard version"
