5ab387af3060f6ae94c98cd72079e7459c7a9221
cyroth <cyroth@users.noreply.github.com>
2018-11-26 15:35:32 +1000

    Update install-forge.sh
    copy instead of move

--- install-forge.sh
+++ install-forge.sh
@@ -1,8 +1,8 @@
 sudo yum install -y java-1.8.0-openjdk screen git -y
 sudo yum remove java-1.7.0-openjdk -y
 java -jar forge-*-installer.jar --installServer
-mv minecraft-server-setup-aws/ops.json ./
-mv minecraft-server-setup-aws/eula.txt ./
+cp minecraft-server-setup-aws/ops.json ./
+cp minecraft-server-setup-aws/eula.txt ./
 sudo mv minecraft-server-setup-aws/minecraft.init /etc/init.d/minecraft
 sudo chmod +x /etc/init.d/minecraft
 sudo chkconfig --add minecraft
